88246acdb90fc2a363843d2627128e3a655a902a
danielburghelea <daniel.burghelea@yahoo.com>
2014-02-16 20:37:54 +0200

    Update functii.js

--- functii.js
+++ functii.js
@@ -3,7 +3,7 @@ var FillForm = function(res){
 			res.write('Connect to mySql\n');
 				res.end([
 				'<form method="POST" action="/auth">'
-				, '<h1>My form</h1>'
+				, '<h1>form</h1>'
 				, '<fieldset>'
 				, '<label>Name</label>'
 				, '<input type="text" name="name"><br>'
@@ -11,21 +11,30 @@ var FillForm = function(res){
 				, '<input type="password" name="password">'
 				, '<p><button>Submit</button></p>'
 				, '</form>'
-				].join(''));
+				].join(''));				
 };
 		
 module.exports.FillForm = FillForm;
 
-
-var GetUserQ =	function (connection){
-					var GetUserQ = connection.query('SELECT * FROM users WHERE users.on = 1');
+var GetUserQ =	function (connection, res, usern){
+		var GetUserQ = connection.query('SELECT * FROM users WHERE users.on = 1 ORDER BY nume');
 			
-						GetUserQ.on('result', function (rows) {
-							console.log(rows);
-						});
+		GetUserQ.on('result', function (rows) {
+				console.log(rows);
+				res.write(JSON.stringify(rows.nume));
+				res.write('<br>');
+		});
 };
 
 module.exports.GetUserQ = GetUserQ;
+
+
+var Logout = function(connectionn, usern){
+	connection.query('UPDATE users SET on = :on WHERE nume = :nume',
+                     {on: 0, nume: usern});
+};
+
+module.exports.Logout = Logout;
 
 
 var NewUser = function (connection, user){
@@ -33,3 +42,17 @@ var NewUser = function (connection, user){
 };
 
 module.exports.NewUser = NewUser;
+
+
+var User = function (name) {
+  this.nume = name;
+};
+
+module.exports.User = User;
+
+
+var GetName = function() {
+	return this.nume;
+}
+
+module.exports.GetName = GetName;
