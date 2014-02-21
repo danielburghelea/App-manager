7aa3ba3c56b080f85a8eec4fbd6c368e94023441
daniel b <daniel.burghelea@yahoo.com>
2014-02-21 23:00:40 +0200

    queries on psql

--- UserLog.js
+++ UserLog.js
@@ -1,38 +1,17 @@
-exports.FillForm = function(res){ // login form
-			res.writeHead(200, { 'Content-Type': 'text/html' });
-				res.end([
-				'<form method="POST" action="/auth">'
-				, '<h1>form</h1>'
-				, '<fieldset>'
-				, '<label>Name</label>'
-				, '<input type="text" name="name"><br>'
-				, '<label>Password</label>'
-				, '<input type="password" name="password">'
-				, '<p><button>Submit</button></p>'
-				, '</form>'
-				].join(''));				
-};
-		
-exports.GetUserQ =	function (connection, res, usern){ //get online (on = 1) user from DB
-	connection.query('SELECT * FROM users WHERE users.on = 1 ORDER BY nume')
-			
-		.on('result', function (rows) {
-		connection.pause();
-			res.write(JSON.stringify(rows.nume));
-			console.log(rows);
-			res.write('<br>');
-		connection.resume();
-		}).on('end', function (){
-			res.end('<br><a href=/logout>Logout</a>');
-		});
-};
-
-exports.Logout = function(connection, usern){ //set on to 0 when logout
-	connection.query("UPDATE users SET users.on = "+0+" WHERE users.nume ='" +usern+"'");
-};
-
-exports.NewUser = function (connection, user){ // add new user
-	connection.query('INSERT INTO users SET ?', user);
+exports.LoginForm = function(res){ // login form
+	res.writeHead(200, { 'Content-Type': 'text/html' });
+		res.write([
+		'<form method="POST" action="/auth">'
+		, '<h1>Login area</h1>'
+		, '<fieldset>'
+		, '<label>Name</label>'
+		, '<input type="text" name="name"><br>'
+		, '<label>Password</label>'
+		, '<input type="password" name="password">'
+		, '<p><button>Submit</button></p>'
+		, '</form>'
+		].join(''));
+	res.end();
 };
 
 exports.User = function (name) { // setter for user
@@ -41,4 +20,26 @@ exports.User = function (name) { // setter for user
 
 exports.User.prototype.GetName = function() { // getter for user
 	return this.nume;
+};
+
+exports.GetUserQ = function(client, res, name){ //get all online users but himself
+	client.query('SELECT * FROM users WHERE users.on_p = 1 AND name <> $1 ORDER BY name', [name])
+
+		.on('row', function(row) {
+			console.log(JSON.stringify(row));
+			res.write(JSON.stringify(row));
+			res.write('<br>');
+		})
+		.on('end', function() {
+			res.write('****');
+			res.end('<br><a href=/logout>Logout</a>'); //link to logout
+		});
+};
+
+exports.NewUserQ = function (client, user){ // add new user
+	client.query('INSERT INTO users(name, on_p) values($1, $2)', [user, 1]);
+};
+
+exports.LogoutQ = function(client, usern){ //set on to 0 when logout
+	client.query('UPDATE users SET on_p = $1 WHERE name =$2', [0, usern]);
 };
